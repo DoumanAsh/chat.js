38a730ad016c6c450ccd2cfc3f5bcf631377b44c
Douman <douman@gmx.se>
2016-03-15 22:06:15 +0300

    Initial

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http)
+const PORT = 8080;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use('/', express.static(__dirname + '/static'));
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Some real time chat'
+    });
+});
+
+io.on('connection', function(socket) {
+    console.log('>a user connected');
+    io.emit('enter', null);
+
+    socket.on('disconnect', function(){
+        console.log('>user disconnected');
+        io.emit('left', null);
+    });
+
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    });
+});
+
+http.listen(PORT, function () {
+    console.log('Start chat.js on port ' + PORT);
+});
---
+++ static/chat.js
@@ -0,0 +1,32 @@
+var socket = io()
+var form;
+var message_box;
+function submit() {
+    socket.emit('chat message', form.msg.value);
+    form.msg.value = ""
+}
+
+function createMsg(msg) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(msg));
+    return li;
+}
+
+window.onload = function() {
+    form = document.getElementById('ff');
+    message_box = document.getElementById('messages');
+
+    form.addEventListener("submit", submit);
+
+    socket.on('left', function(msg) {
+        message_box.appendChild(createMsg(">Someone left chat :("));
+    });
+
+    socket.on('enter', function(msg){
+        message_box.appendChild(createMsg(">Someone entered chat"));
+    });
+
+    socket.on('chat message', function(msg){
+        message_box.appendChild(createMsg(msg));
+    });
+}
